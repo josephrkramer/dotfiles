a645c237d53fc290e3151b22bcedf09cfaf5559f
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-07-21 18:47:53 +0000

    init and update submodule

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # dotfiles
 My codespaces config
 
+## Currently
+Will initialize and update submodules
+
+## Previously
 This will currently setup and install the Google Cloud SDK to be used with the Gemini VS Code extension.
--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 #Setup Google Cloud SDK for Gemini
-curl https://sdk.cloud.google.com | bash
+#curl https://sdk.cloud.google.com | bash
+
+#Init and update git submodules
+git submodule init
+git submodule update
