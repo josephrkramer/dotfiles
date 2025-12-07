4818d6a43f5c726a422ebbe4c5afb8b631d95a54
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2025-12-07 10:27:14 -0500

    install chrome for antigravity

---
+++ chromeos/install-chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
