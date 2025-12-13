32d22a5830cbadd8e154efc862dfdda311811bf2
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2025-12-13 15:28:24 -0500

    install tripo

---
+++ chromeos/install-tripo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install -y python3-pip
+
+cd ~
+
+git clone https://github.com/VAST-AI-Research/TripoSR.git
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+
+pip install --upgrade setuptools
+pip install -r requirements.txt
